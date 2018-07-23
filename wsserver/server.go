fd5f200efd2f4b26d1cfe82fd557f0aac1f6ae1f
Tomas Machalek <tomas.machalek@gmail.com>
2018-07-23 15:53:33 +0200

    Fix tasks sync. and structure

--- wsserver/server.go
+++ wsserver/server.go
@@ -17,11 +17,13 @@
 package wsserver
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/czcorpus/kontext-atn/kcache"
 	"github.com/gorilla/websocket"
@@ -31,13 +33,16 @@ import (
 // required by kontext-atn to run the
 // embedded WebSocket server.
 type Config struct {
-	Address     string `json:"address"`
-	URLPathRoot string `json:"urlPathRoot"`
+	Address        string   `json:"address"`
+	URLPathRoot    string   `json:"urlPathRoot"`
+	AllowedOrigins []string `json:"allowedOrigins"`
 }
 
 // WSServer handles HTTP/WebSocket requests/connections defined for kontex-atn
 type WSServer struct {
 	conf          *Config
+	httpServer    *http.Server
+	mux           *http.ServeMux
 	hub           *Hub
 	cacheRootPath string
 }
@@ -45,28 +50,38 @@ type WSServer struct {
 // NewWSServer creates a properly initialized
 // instance of WSServer
 func NewWSServer(hub *Hub, conf *Config, cacheRootPath string) *WSServer {
+	mux := http.NewServeMux()
 	ans := &WSServer{
 		conf:          conf,
+		mux:           mux,
+		httpServer:    &http.Server{Addr: conf.Address, Handler: mux},
 		hub:           hub,
 		cacheRootPath: cacheRootPath,
 	}
-	ans.init()
-	return ans
-}
 
-func (s *WSServer) init() {
-	if !strings.HasPrefix(s.conf.URLPathRoot, "/") {
+	if !strings.HasPrefix(ans.conf.URLPathRoot, "/") {
 		log.Fatal("URLPathRoot must start with /")
 	}
-	http.HandleFunc(s.conf.URLPathRoot, s.serveHome)
-	http.HandleFunc(s.conf.URLPathRoot+"/ws", s.serveNotifier)
+	ans.mux.HandleFunc(conf.URLPathRoot+"/", ans.serveHome)
+	ans.mux.HandleFunc(conf.URLPathRoot+"/ws", ans.serveNotifier)
+
+	return ans
 }
 
 // Serve starts the server and blocks until
 // it is closed.
 func (s *WSServer) Serve() {
 	log.Printf("INFO: Serving at %s", s.conf.Address+s.conf.URLPathRoot)
-	http.ListenAndServe(s.conf.Address, nil)
+	s.httpServer.ListenAndServe()
+	http.ListenAndServe(s.conf.Address, s.mux)
+}
+
+// Shutdown gracefully stops the server
+func (s *WSServer) Shutdown() {
+	log.Print("INFO: Shutting down the web server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	s.httpServer.Shutdown(ctx)
 }
 
 func (s *WSServer) serveNotifier(writer http.ResponseWriter, request *http.Request) {
@@ -74,7 +89,13 @@ func (s *WSServer) serveNotifier(writer http.ResponseWriter, request *http.Reque
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 		CheckOrigin: func(r *http.Request) bool {
-			return true // TODO !!!
+			origin := r.Header.Get("Origin")
+			for _, allowedOrigin := range s.conf.AllowedOrigins {
+				if origin == allowedOrigin {
+					return true
+				}
+			}
+			return false
 		},
 	}
 	conn, err := upgrader.Upgrade(writer, request, nil)
@@ -93,8 +114,9 @@ func (s *WSServer) serveNotifier(writer http.ResponseWriter, request *http.Reque
 	s.hub.Register <- NewClient(cacheIdent, s.hub, conn)
 }
 
+// serveHome provides some information about running server
 func (s *WSServer) serveHome(writer http.ResponseWriter, request *http.Request) {
-	if request.URL.Path != s.conf.URLPathRoot {
+	if request.URL.Path != s.conf.URLPathRoot+"/info" {
 		http.Error(writer, "Not found", http.StatusNotFound)
 		return
 	}
